6de847d497a267b31e02eeab56ae60eb90d459f1
Yangolas <yanguasuah@gmail.com>
2024-03-29 12:14:09 +0100

    ultimos cambios

---
+++ src/3.PlcInspector.Models/AlmCrono.cs
@@ -0,0 +1,18 @@
+﻿
+namespace _3.PlcInspector.Models;
+
+public class AlmCrono
+{
+    public string Campo1 { get; set; } = null!;
+    public string Campo2 { get; set; } = null!;
+    public string Campo3 { get; set; } = null!;
+    public string Campo4 { get; set; } = null!;
+    public string Campo5 { get; set; } = null!;
+    public string Campo6 { get; set; } = null!;
+    public string Campo7 { get; set; } = null!;
+    public string Campo8 { get; set; } = null!;
+    public string Campo9 { get; set; } = null!;
+    public string Campo10 { get; set; } = null!;
+    public string Campo11 { get; set; } = null!;
+    public string Campo12 { get; set; } = null!;
+}
